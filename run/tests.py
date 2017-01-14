67e723eaf9586813595007436cf67a312cd099ac
altostratous <altostratous@gmail.com>
2017-01-14 10:06:23 +0330

    Added some failed test.

--- run/tests.py
+++ run/tests.py
@@ -1,3 +1,15 @@
+import time
 from django.test import TestCase
+from .models import Run
+from . import views
+
 
 # Create your tests here.
+class TestRunAPI(TestCase):
+    def setUp(self):
+        self.run_1 = Run(start_time=time.time())
+        self.run_1.save()
+
+    def test_report_view(self):
+        response = self.client.post(views.report, '{"from_time":"0-0-0"}')
+        print(response)
