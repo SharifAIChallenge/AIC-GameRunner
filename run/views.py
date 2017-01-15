5a6ed303079738e7062f53b91b074c2d33b365c7
altostratous <altostratous@gmail.com>
2017-01-16 00:51:28 +0330

    Tried to fix mentioned issues.

--- run/views.py
+++ run/views.py
@@ -1,5 +1,5 @@
 from rest_framework.parsers import JSONParser
-from .serializers import RunSerializer
+from .serializers import RunCreateSerializer, RunReportSerializer
 from .models import Run
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,23 +9,28 @@ from rest_framework.views import APIView
 class RunCreateView(APIView):
     parser_classes = (JSONParser,)
 
-    @staticmethod
-    def post(request):
+    def post(self, request):
         runs_json = JSONParser().parse(request)
-        error_messages = []
-        created_runs = []
+        # validate if the data is a list
+        if runs_json is not list:
+            return Response({'result': None}, status=status.HTTP_400_BAD_REQUEST)
+        result = []
         for run_json in runs_json:
-            serializer = RunSerializer(data=run_json)
+            serializer = RunCreateSerializer(data=run_json)
             if serializer.is_valid():
                 run = serializer.save()
-                created_runs.append(run.id)  # We will replace id by token
+                result.append(run.id)  # We will replace id by token
             else:
-                error_messages.append(serializer.error_messages)
-        return Response({'error_messages': error_messages,
-                         'created_runs': created_runs}, status=status.HTTP_207_MULTI_STATUS)
+                result.append(serializer.error_messages)
+        return Response({'result': result}, status=status.HTTP_207_MULTI_STATUS)
 
-    @staticmethod
-    def get(request):
+
+class RunReportView(APIView):
+    def get(self, request):
         data = JSONParser().parse(request)
         runs = Run.objects.all().query(end_time_gte=data['from_time'])
-        return Response(runs)
+        result = []
+        for run in runs:
+            serializer = RunReportSerializer(run)
+            result.append(serializer.data)
+        return Response(result)
