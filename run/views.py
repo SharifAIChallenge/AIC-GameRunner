1197944519c5604d2aef009286557aac119c22f1
altostratous <altostratous@gmail.com>
2017-01-15 22:51:38 +0330

    Tried to fix mentioned things.

--- run/views.py
+++ run/views.py
@@ -1,25 +1,31 @@
-from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from .serializers import RunSerializer
 from .models import Run
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
 
-@api_view(http_method_names=['POST'])
-def run(request):
-    runs_json = JSONParser().parse(request)
-    data_with_error = []
-    for run_json in runs_json:
-        serializer = RunSerializer(data=run_json)
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            data_with_error.append(run_json)
-    return Response({'data_with_error': data_with_error})
+class RunCreateView(APIView):
+    parser_classes = (JSONParser,)
 
+    @staticmethod
+    def post(request):
+        runs_json = JSONParser().parse(request)
+        error_messages = []
+        created_runs = []
+        for run_json in runs_json:
+            serializer = RunSerializer(data=run_json)
+            if serializer.is_valid():
+                run = serializer.save()
+                created_runs.append(run.id)  # We will replace id by token
+            else:
+                error_messages.append(serializer.error_messages)
+        return Response({'error_messages': error_messages,
+                         'created_runs': created_runs}, status=status.HTTP_207_MULTI_STATUS)
 
-@api_view(http_method_names=['GET'])
-def report(request):
-    data = JSONParser().parse(request)
-    runs = Run.objects.all().query(end_time_gte=data['from_time'])
-    return Response(runs)
+    @staticmethod
+    def get(request):
+        data = JSONParser().parse(request)
+        runs = Run.objects.all().query(end_time_gte=data['from_time'])
+        return Response(runs)
