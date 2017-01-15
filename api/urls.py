bde48cab877a090517e5d5427b05a5985969e278
Shervin Khastoo <shervinkh145@gmail.com>
2017-01-16 01:03:57 +0330

    Add get file (#3)

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'api'
 urlpatterns = [
     url(r'^storage/new_file$', views.storage.FileUploadView.as_view(), name='create_file'),
+    url(r'^storage/get_file$', views.storage.FileDownloadView.as_view(), name='get_file'),
 ]
--- api/views/storage.py
+++ api/views/storage.py
@@ -1,8 +1,11 @@
+import os
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.parsers import FormParser, MultiPartParser
 from storage.serializers import FileSerializer
+from storage.models import File
 
 
 class FileUploadView(APIView):
@@ -18,3 +21,20 @@ class FileUploadView(APIView):
             return Response({'token': serializer.data['id']}, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FileDownloadView(APIView):
+    def post(self, request):
+        if 'token' in request.data:
+            try:
+                file = File.objects.get(id=request.data['token'])
+                file_name = os.path.basename(file.file.name)
+                response = HttpResponse(file.file, content_type='application/octet-stream')
+                response['Content-Disposition'] = 'attachment; filename="%s"' % file_name
+                return response
+            except File.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            except ValueError:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
