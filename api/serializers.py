39895ba0be0eb0239138f31858bed9d3aa4baeee
altostratous <altostratous@gmail.com>
2017-02-05 21:33:15 +0330

    Run api with game models (#7)

---
+++ api/serializers.py
@@ -0,0 +1,80 @@
+from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+
+from game.models import Operation, OperationParameter
+from run.models import Run
+from run.models import ParameterValue
+from storage.models import File
+from storage.serializers import FileSerializer
+
+
+# class FilePathSerializer(serializers.ModelSerializer):
+#     file = FileSerializer(many=False)
+#
+#     # definition = FileDefinitionSerializer()
+#
+#     class Meta:
+#         model = FilePath
+#         fields = ('file',
+#                   # 'definition',
+#                   )
+
+class ParameterValueSetSerializerField(serializers.Field):
+    def to_representation(self, value):
+        result = {}
+        for parameter_value in value:
+            # todo serialize file definition
+            result[parameter_value.parameter.name] = parameter_value._value
+        return result
+
+    def to_internal_value(self, data):
+        internal_value = []
+        if not isinstance(data, dict):
+            raise serializers.ValidationError('parameters data must be a dictionary.')
+        for parameter in data:
+            internal_value.append((OperationParameter.objects.get(name=parameter), data[parameter]))
+        return internal_value
+
+
+class OperationSerializerFiled(serializers.Field):
+    def to_internal_value(self, data):
+        return Operation.objects.get(name=data)
+
+    def to_representation(self, value):
+        return value.name
+
+
+class RunReportSerializer(serializers.ModelSerializer):
+    parameters = ParameterValueSetSerializerField(read_only=True)
+    operation = OperationSerializerFiled()
+
+    class Meta:
+        model = Run
+        fields = (
+            'id', 'operation', 'status', 'end_time', 'log', 'parameters')
+        read_only_fields = fields
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['parameters'] = ParameterValueSetSerializerField().to_representation(
+            instance.parameter_value_set.all().filter(parameter__is_input=False))
+        return data
+
+
+class RunCreateSerializer(serializers.Serializer):
+    parameters = ParameterValueSetSerializerField()
+    operation = OperationSerializerFiled()
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError()
+
+    def create(self, validated_data):
+        run = Run(operation=validated_data['operation'])
+        run.save()
+        for parameter_value_data in validated_data['parameters']:
+            parameter_value = ParameterValue(parameter=parameter_value_data[0], _value=parameter_value_data[1])
+            parameter_value.run = run
+            parameter_value.save()
+        run.parameter_set = validated_data['parameters']
+        run.save()
+        return run
---
+++ run/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from storage.models import File
+from game_runner import settings
+from game.models import OperationParameter, Game, Operation
+import uuid
+
+
+class ParameterValue(models.Model):
+    _value = models.TextField()
+    run = models.ForeignKey('Run', related_name='parameter_value_set')
+    parameter = models.ForeignKey(OperationParameter)
+
+    @property
+    def value(self):
+        if self.parameter.type == OperationParameter.PARAMETER_TYPES['string']:
+            return self._value
+        else:
+            return File.objects.get(id=self._value)
+        return None
+
+    @value.setter
+    def value(self, value):
+        if self.parameter.type == OperationParameter.PARAMETER_TYPES['string']:
+            self._value = value
+        else:
+            if not isinstance(value, File):
+                raise ValueError()
+            self._value = value.pk
+
+
+class Run(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    operation = models.ForeignKey(Operation)
+    request_time = models.DateTimeField(auto_now_add=True, null=True)
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+    log = models.TextField()
+    # choices for the status
+    PENDING = 3
+    SUCCESS = 1
+    FAILURE = 2
+    status_choices = (
+        (SUCCESS, 'Success'),
+        (FAILURE, 'Failure'),
+        (PENDING, 'Pending'),
+    )
+    status = models.SmallIntegerField(choices=status_choices, default=PENDING)
+
