0b3f13c8097d41526af264a00b85d86a1aa75340
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-02-08 21:10:07 +0330

    Added running section

--- run/models.py
+++ run/models.py
@@ -11,6 +11,12 @@ import os
 import shutil
 import yaml
 
+import docker
+from docker.types import Resources as DockerResources
+from docker.types import RestartPolicy as DockerRestartPolicy
+
+import time
+
 
 class ParameterValue(models.Model):
     _value = models.TextField()
@@ -35,19 +41,20 @@ class ParameterValue(models.Model):
                 raise ValueError()
             self._value = value.pk
 
-    def compile_for_input(self, parameter_path):
+    def compile_for_input(self, dir):
         """
         If the parameter is a string, this method
-        stores it in the parameter_path.
-        If the parameter is a file, it copies
-        the file to the given path.
+        returns the value
+        If the parameter is a file, it copies it to
+        a file inside `dir` and returns the path.
         """
         if self.parameter.is_input:
             if self.parameter.type == 'string':
-                with open(parameter_path, "w") as file:
-                    file.write(self.value)
+                return self.value
             elif self.parameter.type == 'file':
+                parameter_path = os.path.join(dir, self.parameter.name)
                 shutil.copy(self.value.name, parameter_path)
+                return parameter_path
         raise AssertionError("Unsupported parameter")
 
 
@@ -78,6 +85,9 @@ class Run(models.Model):
         self.save()
 
         COMPOSE_FILE_NAME = "docker-compose.yml"
+        MANAGER_CPU_LIMIT = None
+        MANAGER_MEMORY_LIMIT = None
+        STATUS_CHECK_PERIOD = 10
 
         with tempfile.TemporaryDirectory(prefix=settings.NFS_DIR) as shared_path:
 
@@ -99,23 +109,33 @@ class Run(models.Model):
                 context = {}
                 output_parameters = []
                 for parameter in self.operation.parameters.all():
-                    parameter_path = os.path.join(shared_path, self.parameter.name)
                     if parameter.is_input:
                         try:
                             value = self.parameter_value_set.get(parameter=parameter)
-                            value.compile_for_input(parameter_path)
+                            template_value = value.compile_for_input(shared_path)
                         except ParameterValue.DoesNotExist:
                             if parameter.is_required:
                                 raise AssertionError("Input parameter {} is required but is not given".format(parameter.name))
                             else:
-                                parameter_path = "/dev/null"
+                                template_value = parameter.get_non_existant_value()
                     else:
                         try:
                             self.parameter_value_set.get(parameter=parameter).delete()
                         except ParameterValue.DoesNotExist:
                             pass
                         output_parameters.append(parameter)
-                    context[parameter.name] = parameter_path
+                        template_value = os.path.join(shared_path, self.parameter.name)
+                    context[parameter.name] = template_value
+
+                # TODO: Resources are used for different purposes. Separate them.
+
+                for resource in self.operation.resources.all():
+                    resource_path = os.path.join(shared_path, resource.name)
+                    shutil.copyfile(
+                        resource.file.name,
+                        resource_path
+                    )
+                    context[resource.name] = resource_path
 
                 # Section 3: Compile config file
                 template_engine = Engine(
@@ -130,6 +150,9 @@ class Run(models.Model):
                     file.write(rendered_compose_file)
 
             # Section 4: Provide config file to the manager node and run it
+
+            # TODO: Use docker-compose interface to calculate sum of limits
+
             memlim_sum = 0.0
             cpulim_sum = 0.0
             with open(compiled_compose_file_path) as file:
@@ -139,18 +162,46 @@ class Run(models.Model):
                     cpulim_sum += float(x['cpus'])
                     s = x['memory']
                     if __name__ == '__main__':
-                        if s[-1] == 'K':
+                        if s[-1].upper() == 'B':
                             memlim_sum += int(s[:-1])
-                        elif s[-1] == 'M':
-                            memlim_sum += int(s[-1]) * 1024
-                        elif s[-1] == 'G':
+                        if s[-1].upper() == 'K':
+                            memlim_sum += int(s[:-1]) * 1024
+                        elif s[-1].upper() == 'M':
                             memlim_sum += int(s[-1]) * 1024 * 1024
+                        elif s[-1].upper() == 'G':
+                            memlim_sum += int(s[-1]) * 1024 * 1024 * 1024
                         else:
                             raise TypeError
 
-                # docker service create ...
+                client = docker.from_env()
+                manager = client.services.create(
+                    image='kondor-manager',
+                    resources=DockerResources(
+                        cpu_limit=(MANAGER_CPU_LIMIT * (10 ** 9))
+                        if MANAGER_CPU_LIMIT is not None else None,
+                        mem_limit=MANAGER_MEMORY_LIMIT,
+                        cpu_reservation=(
+                                            (MANAGER_CPU_LIMIT
+                                             if MANAGER_CPU_LIMIT is not None else 0) +
+                                            cpulim_sum
+                                        ) * (10 ** 9),
+                        mem_reservation=(
+                            (MANAGER_MEMORY_LIMIT
+                             if MANAGER_MEMORY_LIMIT is not None else 0) +
+                            memlim_sum
+                        ),
+                    ),
+                    restart_policy=DockerRestartPolicy(
+                        condition='none'
+                    ),
+                    mounts=[
+                        "{}:/compose:ro".format(shared_path)
+                    ]
+                )
 
-                #while [ 1 -ne `docker service ps $name | tail -n +2 | awk '{print $5}' | grep Shutdown | wc -l` ] ; do sleep 0.5; done
+                # TODO: Use docker interface to wait for the manager
+                while len(manager.tasks(filter={'desired-state': 'shutdown'})) == 0:
+                    time.sleep(STATUS_CHECK_PERIOD)
 
         # Section 5: Save outputs. Set run status to success
         for parameter in output_parameters:
