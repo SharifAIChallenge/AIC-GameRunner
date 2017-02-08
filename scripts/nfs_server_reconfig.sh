1d253bd14bbbfc37056d94cb27a9685e5982dd93
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-02-08 21:38:38 +0330

    Added nfs scripts

---
+++ scripts/nfs_server_reconfig.sh
@@ -0,0 +1,22 @@
+if [ "$(whoami)" != "root" ]
+then
+    echo "Run me as root"
+    exit 1
+fi
+
+if [ "$#" != "2" ]
+then
+    echo "NFS Server Reconfigure Script!"
+    echo "Usage: $0 nfs_directory client_ips"
+    echo "Example: $0 /data/nfs '78.47.157.48 192.168.1.1'"
+    exit 1
+fi
+
+printf "$1" > /etc/exports
+for ip in $2
+do
+    printf " $ip(rw,sync,no_subtree_check)" >> /etc/exports
+done
+echo '' >> /etc/exports
+
+systemctl restart nfs-kernel-server
