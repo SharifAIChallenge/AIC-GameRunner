dbcdb2bd27209b64e61ef5b472dcbdac75bdffad
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-02-09 17:52:50 +0000

    Added admin action to execute run

--- run/admin.py
+++ run/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Run, ParameterValue
 
 # Register your models here.
+
+class RunAdmin(admin.ModelAdmin):
+    actions = ['execute']
+    
+    def execute(self, request, queryset):
+        for run in queryset:
+            run.compile_compose_file()
+
+admin.site.register(Run, RunAdmin)
+admin.site.register(ParameterValue)
