fc5e2261bd7d2fd21225547b23078088f51af294
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-02-10 18:22:51 +0330

    Fixed mount syntax

--- run/models.py
+++ run/models.py
@@ -15,6 +15,8 @@ import yaml
 import docker
 from docker.types import Resources as DockerResources
 from docker.types import RestartPolicy as DockerRestartPolicy
+from docker.types import Mount as DockerMount
+
 
 import time
 
@@ -210,8 +212,18 @@ class Run(models.Model):
                     condition='none'
                 ),
                 mounts=[
-                    "{}:/compose:ro".format(shared_path),
-                    "/var/run/docker.sock:/var/run/docker.sock:ro",
+                    DockerMount(
+                        source=shared_path,
+                        target="/compose",
+                        type='bind',
+                    ),
+                    DockerMount(
+                        # TODO: Can we use a more general term here e.g. settings.DOCKER_HOST?
+                        source='/var/run/docker.sock',
+                        target='/var/run/docker.sock',
+                        type='bind',
+                        read_only=True
+                    ),
                 ],
                 name="{}".format(manager_uid),
             )
