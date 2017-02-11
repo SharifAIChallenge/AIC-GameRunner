2d258566f8e9fe5672715d7c594f33e6e70efb85
Amir Keivan Mohtashami <akmohtashami@users.noreply.github.com>
2017-02-11 22:05:51 +0330

    ًRevised registry scripts (#12)

---
+++ scripts/create_registry_certificate.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+# Based on
+# https://gist.github.com/jhamrick/ac0404839b5c7dab24b5
+
+
+if [[ $# -ne 1 ]];
+then
+    echo "Usage: create_registry_certificate.sh <ip>"
+    exit 1
+fi
+
+ROOT="$(pwd)/certs"
+CAFILE="${ROOT}/ca.crt"
+CAKEY="${ROOT}/ca.key"
+PASSFILE="${ROOT}/pass"
+PASSOPT="file:$PASSFILE"
+
+echo "Cleaning previous certificates"
+rm -rf $ROOT
+mkdir -p $ROOT
+
+cat > ${PASSFILE} <<EOF
+hello_world
+hello_world
+EOF
+
+cp openssl.cnf ${ROOT}/openssl-tmp.cnf
+echo "IP.1 = $1" >> ${ROOT}/openssl-tmp.cnf
+
+
+echo ">> generating a certificate authority"
+  openssl genrsa -des3 \
+  -passout ${PASSOPT} \
+    -out ${CAKEY} 2048
+  openssl req -new -x509 -days 365 \
+    -batch \
+    -passin ${PASSOPT} \
+    -passout ${PASSOPT} \
+    -key ${CAKEY} \
+    -out ${CAFILE}
+echo "<< certificate authority generated."
+
+echo ".. key"
+  openssl genrsa -des3 \
+  -passout ${PASSOPT} \
+  -out ${ROOT}/registry.key 2048
+
+echo ".. request"
+  openssl req -subj "/CN=${HOSTNAME}" -new \
+    -batch \
+    -key ${ROOT}/registry.key \
+    -out ${ROOT}/req.csr \
+    -config ${ROOT}/openssl-tmp.cnf \
+    -passin ${PASSOPT} \
+    -passout ${PASSOPT}
+
+ echo ".. certificate"
+  openssl x509 -req -days 365 \
+    -passin ${PASSOPT} \
+    -in ${ROOT}/req.csr \
+    -CA ${CAFILE} \
+    -CAkey ${CAKEY} \
+    -CAcreateserial \
+    -extensions v3_req \
+    -extfile ${ROOT}/openssl-tmp.cnf \
+    -out ${ROOT}/registry.crt \
+
+  echo ".. removing key password"
+  openssl rsa \
+    -in ${ROOT}/registry.key \
+    -out ${ROOT}/registry.key \
+    -passin ${PASSOPT} \
+    -passout ${PASSOPT}
+
+echo "Cleaning up..."
+rm ${ROOT}/openssl-tmp.cnf
+
+echo "###################################################"
+echo "DONE. "
+echo "ca.crt must be provided to clients."
+echo "registry.key and registry.crt must be used to start the registry."
--- scripts/secure_docker_start_registry.sh
+++ scripts/secure_docker_start_registry.sh
@@ -2,39 +2,40 @@
 echo "Removing current registry if exists."
 sudo docker rm -f /registry
 
-mkdir -p certs && openssl req \
- -newkey rsa:4096 -nodes -sha256 -keyout certs/$1.key \
- -x509 -days 365 -out certs/$1.crt -config openssl.cnf
-
-if [ $# -eq 2 ]
+if [ $# -eq 1 ]
 	then
 		if [ $2 == "pull_through_cache" ]
 			then
 				echo "Starting Registry As A Pull Through Cache."
 				sudo docker run\
-				 -d\
-				 -p 5000:5000\
-				 -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io\
-                 -v `pwd`/certs:/certs\
-                 -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/$1.crt\
-                 -e REGISTRY_HTTP_TLS_KEY=/certs/$1.key\
-                 -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
-				 --restart=always\
-				 --name registry\
-				 registry:2
+                -d\
+                -p 5000:5000\
+                -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io\
+                -v `pwd`/certs:/certs\
+                -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt\
+                -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key\
+                -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
+                --restart=always\
+                --name registry\
+                registry:2
 			else
 				echo "Bad argument provided."
 		fi
-	else
-                echo "Starting Registry."
-                sudo docker run\
-                 -d\
-				 -p 5000:5000\
-				-p 5001:5001\
-                 -v `pwd`/certs:/certs\
-                 -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/$1.crt\
-                 -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
-                 --restart=always\
-                 --name registry\
-		  registry:2
+elif [ $# -eq 0 ]
+then
+            echo "Starting Registry."
+            sudo docker run\
+            -d\
+            -p 5000:5000\
+            -p 5001:5001\
+            -v `pwd`/certs:/certs\
+            -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt\
+            -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key\
+            -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
+            --restart=always\
+            --name registry\
+            registry:2
+else
+    echo "Usage: secure_docker_start_registry.sh <certificate_name> [pull_through_cache]"
+    exit 1
 fi
