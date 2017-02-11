c14a17312dbc01d5ca7e2bbbbe00a57a881bfd40
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-02-11 22:01:25 +0330

    Finalized scripts for creating a registry

--- scripts/create_registry_certificate.sh
+++ scripts/create_registry_certificate.sh
@@ -19,9 +19,63 @@ echo "Cleaning previous certificates"
 rm -rf $ROOT
 mkdir -p $ROOT
 
+cat > ${PASSFILE} <<EOF
+hello_world
+hello_world
+EOF
+
 cp openssl.cnf ${ROOT}/openssl-tmp.cnf
 echo "IP.1 = $1" >> ${ROOT}/openssl-tmp.cnf
 
-openssl req \
- -newkey rsa:4096 -nodes -sha256 -keyout ${ROOT}/registry.key \
- -x509 -days 365 -out ${ROOT}/registry.crt -extensions v3_req -config  ${ROOT}/openssl-tmp.cnf
+
+echo ">> generating a certificate authority"
+  openssl genrsa -des3 \
+  -passout ${PASSOPT} \
+    -out ${CAKEY} 2048
+  openssl req -new -x509 -days 365 \
+    -batch \
+    -passin ${PASSOPT} \
+    -passout ${PASSOPT} \
+    -key ${CAKEY} \
+    -out ${CAFILE}
+echo "<< certificate authority generated."
+
+echo ".. key"
+  openssl genrsa -des3 \
+  -passout ${PASSOPT} \
+  -out ${ROOT}/registry.key 2048
+
+echo ".. request"
+  openssl req -subj "/CN=${HOSTNAME}" -new \
+    -batch \
+    -key ${ROOT}/registry.key \
+    -out ${ROOT}/req.csr \
+    -config ${ROOT}/openssl-tmp.cnf \
+    -passin ${PASSOPT} \
+    -passout ${PASSOPT}
+
+ echo ".. certificate"
+  openssl x509 -req -days 365 \
+    -passin ${PASSOPT} \
+    -in ${ROOT}/req.csr \
+    -CA ${CAFILE} \
+    -CAkey ${CAKEY} \
+    -CAcreateserial \
+    -extensions v3_req \
+    -extfile ${ROOT}/openssl-tmp.cnf \
+    -out ${ROOT}/registry.crt \
+
+  echo ".. removing key password"
+  openssl rsa \
+    -in ${ROOT}/registry.key \
+    -out ${ROOT}/registry.key \
+    -passin ${PASSOPT} \
+    -passout ${PASSOPT}
+
+echo "Cleaning up..."
+rm ${ROOT}/openssl-tmp.cnf
+
+echo "###################################################"
+echo "DONE. "
+echo "ca.crt must be provided to clients."
+echo "registry.key and registry.crt must be used to start the registry."
