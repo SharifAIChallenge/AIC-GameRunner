ae96b956770f12fcfe02fa53ceab01c3e1d1ba73
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-02-13 17:53:40 +0330

    Inline admin for operation parameters

--- game/admin.py
+++ game/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from game.models import *
 
+
+class OperationParameterInlineAdmin(admin.TabularInline):
+    model = OperationParameter
+
+
+class OperationAdmin(admin.ModelAdmin):
+    inlines = (OperationParameterInlineAdmin, )
+
 admin.site.register(Game)
-admin.site.register(Operation)
+admin.site.register(Operation, OperationAdmin)
 admin.site.register(OperationParameter)
 admin.site.register(OperationResource)
