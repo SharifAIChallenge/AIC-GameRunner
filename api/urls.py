7a4e770f15e1f45a7cc684b34726a0f30870ce72
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-02-13 20:34:49 +0330

    More general token allowed

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ app_name = 'api'
 urlpatterns = [
     url('^schema/$', views.schema.SchemaView.as_view()),
     url(r'^storage/new_file/$', views.api_storage.FileUploadView.as_view(), name='create_file'),
-    url(r'^storage/get_file/(?P<token>[0-9a-z]+)/$', views.api_storage.FileDownloadView.as_view(), name='get_file'),
+    url(r'^storage/get_file/(?P<token>.+)/$', views.api_storage.FileDownloadView.as_view(), name='get_file'),
     url(r'^run/report/$', views.api_run.RunReportView.as_view(), name='run_report'),
     url(r'^run/run/$', views.api_run.RunCreateView.as_view(), name='create_run'),
 ]
