699d7a9963a3980e46588f3a68ce4698d9e603a5
Iman Gholami <imangholami77@gmail.com>
2020-03-06 14:59:02 +0330

    Fix bugs

--- game_runner/settings.py
+++ game_runner/settings.py
@@ -86,10 +86,6 @@ DATABASES = {
         'HOST': 'db',
         'PORT': '',
     }
-    ,'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 LOGGING = {
