025c2987b8c288b6e136e6621b99190a7f2ed21d
Iman Gholami <imangholami77@gmail.com>
2020-03-08 16:42:29 +0330

    Fix bug

--- run/tasks.py
+++ run/tasks.py
@@ -5,14 +5,12 @@ from django.db import transaction
 from .models import Run
 
 
-@transaction.atomic
 def schedule_sending_response(run):
     if run.response_queue_reference_id is None and run.response == Run.SENDING:
         run.response_queue_reference_id = sending_response.delay(run.pk)
         run.save()
 
 
-@transaction.atomic
 def schedule_run_execution(run):
     if run.queue_reference_id is None and run.status == Run.PENDING:
         run.queue_reference_id = execute_run.delay(run.pk)
@@ -32,12 +30,14 @@ def sending_response(instance_pk):
 
 
 @shared_task
+@transaction.atomic
 def periodic_check_for_missed_runs():
     for missed_run in Run.objects.filter(queue_reference_id__isnull=True, status=Run.PENDING):
         schedule_run_execution(missed_run)
 
 
 @shared_task
+@transaction.atomic
 def periodic_check_for_sending_responses():
     for run in Run.objects.filter(response_queue_reference_id__isnull=True, response=Run.SENDING):
         schedule_sending_response(run)
